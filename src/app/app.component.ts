f4cb073b2d97bcd889baa8cc2fbeeeb70c1b03ba
Jean Oldor Saintus <josaintus@gmail.com>
2023-04-22 23:48:08 -0400

    Création de service expense pour aller chercher les dépenses ou la dépense

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,11 @@ export class AppComponent {
     //   username: 'mathieu.theriault89@me.com',
     //   password: 'a1032010',
     // });
+    this.authService.login({
+      username: 'jean@gmail.com',
+      password: 'jean12345',
+    });
+
     this.http.get('/expenses/all/643dc744faad3a88eab7e7ee').subscribe((res) => {
       console.log(res);
     });
