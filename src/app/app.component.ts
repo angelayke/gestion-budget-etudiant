e57238c1d1ce7f3dfb9cbf95a3fd9f954f22697d
Mathieu Theriault <mathieu.theriault89@gmail.com>
2023-04-24 13:14:48 -0400

    Remove unused dependencies in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { AuthService } from './services/auth.service';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
