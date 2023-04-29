6ec971851dfc146f5a044b3c32da74c6f7613d2f
angelayke <angebee2605@aol.com>
2023-04-29 14:07:59 -0400

    creation accueil - petit probleme a regler de chevauchement de btn

--- src/app/components/accueil/accueil.component.ts
+++ src/app/components/accueil/accueil.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-accueil',
+  selector: 'accueil',
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.scss']
 })
