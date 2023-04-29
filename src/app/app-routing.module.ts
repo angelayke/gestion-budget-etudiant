9807cbb5b8f17967c8baef411a61a882f00339e2
Jean Oldor Saintus <josaintus@gmail.com>
2023-04-29 00:02:08 -0400

    AuthGuard with CanActivate for the routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConnexionComponent } from './components/connexion/connexion.component';
+import { SinscrireComponent } from './components/sinscrire/sinscrire.component';
+import { DepensesComponent } from './components/depenses/depenses.component';
+import { RevenusComponent } from './components/revenus/revenus.component';
+import { AuthGuard } from './guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'connexion', pathMatch: 'full'},
+  {path: 'connexion', component: ConnexionComponent},
+  {path: 'sinscrire', component: SinscrireComponent},
+  {path: 'depenses', component: DepensesComponent, canActivate: [AuthGuard]},
+  {path: 'revenus', component: RevenusComponent , canActivate: [AuthGuard]},
+  {path: '**', component: ConnexionComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router){}
+  canActivate( ): boolean  {
+    if(this.authService.$isAuthenticated){
+      return true;
+    } else {
+      this.router.navigate(['/connexion']);
+      return false;
+    }
+  }
+
+}
