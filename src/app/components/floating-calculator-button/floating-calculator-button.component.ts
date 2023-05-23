5dec43c064badd580e3e10c8946fbf8462c3322a
Mathieu Theriault <mathieu.theriault89@gmail.com>
2023-05-23 18:47:32 -0400

    Correct FloatingCalculatorButtonComponent issue:

--- src/app/components/floating-calculator-button/floating-calculator-button.component.ts
+++ src/app/components/floating-calculator-button/floating-calculator-button.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { appRoutes } from 'src/constants/app-routes.constants';
 import { MatDialog } from '@angular/material/dialog';
 import { CalculatriceComponent } from '../calculatrice/calculatrice.component';
-
 
 @Component({
   selector: 'floating-calculator-button',
@@ -10,16 +8,14 @@ import { CalculatriceComponent } from '../calculatrice/calculatrice.component';
   styleUrls: ['./floating-calculator-button.component.scss'],
 })
 export class FloatingCalculatorButtonComponent implements OnInit {
-  constructor(public dialog: MatDialog) {
-    openDialog() {
-      const dialogRef = this.dialog.open();
-  
-      dialogRef.afterClosed().subscribe(result => {
-        console.log(`Dialog result: ${result}`);
-      });
-    }
-   
+  constructor(public dialog: MatDialog) {}
 
+  openDialog() {
+    const dialogRef = this.dialog.open(CalculatriceComponent);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 
   ngOnInit(): void {}
